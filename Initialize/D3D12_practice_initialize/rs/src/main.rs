6e2e6eb0cc8750d0f7f9d25344590d6541a94158
Hajime-san <utd.c.r.d.s.a@gmail.com>
2020-10-02 04:36:36 +0900

    use D3D12 FFI test

--- Initialize/D3D12_practice_initialize/rs/src/main.rs
+++ Initialize/D3D12_practice_initialize/rs/src/main.rs
@@ -1,23 +1,27 @@
 use winapi::{
     um::{
-        winuser::{RegisterClassW, WNDCLASSW, CS_HREDRAW, CS_VREDRAW,
+        winuser::{ RegisterClassW, WNDCLASSW, CS_HREDRAW, CS_VREDRAW,
                   LoadIconW, IDI_APPLICATION, LoadCursorW, IDC_ARROW,
                   CreateWindowExW, ShowWindow, SW_NORMAL, UpdateWindow,
                   GetMessageW, TranslateMessage, DispatchMessageW, MSG,
-                  WM_DESTROY, PostQuitMessage, DefWindowProcW, WS_OVERLAPPEDWINDOW},
-        wingdi::{GetStockObject, WHITE_BRUSH},
+                  WM_DESTROY, PostQuitMessage, DefWindowProcW, WS_OVERLAPPEDWINDOW },
+        wingdi::{ GetStockObject, WHITE_BRUSH },
     },
     shared::{
-        windef::{HWND, HBRUSH},
-        minwindef::{UINT, WPARAM, LPARAM, LRESULT},
+        windef::{ HWND, HBRUSH },
+        minwindef::{ UINT, WPARAM, LPARAM, LRESULT },
     },
 };
+use winapi::um::{ d3d12, d3d12sdklayers, d3dcommon };
 use std::ptr;
 use std::mem;
 
+const WINDOW_WIDTH: i32 = 1280;
+const WINDOW_HEIGHT: i32 = 720;
+
 fn main() {
     unsafe {
-        let class_name = encode("my_window_class_name");
+        let class_name = encode("DX12Sample");
         if !register_wndclass(&class_name) {
             return;
         }
@@ -26,8 +30,23 @@ fn main() {
         if hwnd.is_null() {
             return;
         }
+
+
+        // initialize Direct3D device
+        let levels: [d3dcommon::D3D_FEATURE_LEVEL; 4] = [
+            d3dcommon::D3D_FEATURE_LEVEL_12_1,
+            d3dcommon::D3D_FEATURE_LEVEL_12_0,
+            d3dcommon::D3D_FEATURE_LEVEL_11_1,
+            d3dcommon::D3D_FEATURE_LEVEL_11_0
+        ];
+
+        println!("{:?}", levels);
+
+
         ShowWindow(hwnd, SW_NORMAL);
         UpdateWindow(hwnd);
+
+
         let mut msg = mem::uninitialized::<MSG>();
         loop {
             if GetMessageW(&mut msg, ptr::null_mut(), 0, 0) == 0 {
@@ -46,7 +65,7 @@ fn encode(source: &str) -> Vec<u16> {
 unsafe fn register_wndclass(class_name: &[u16]) -> bool {
     let mut winc = mem::zeroed::<WNDCLASSW>();
     winc.style = CS_HREDRAW | CS_VREDRAW;
-    winc.lpfnWndProc = Some(win_proc);
+    winc.lpfnWndProc = Some(window_procedure);
     winc.hIcon = LoadIconW(ptr::null_mut(), IDI_APPLICATION);
     winc.hCursor = LoadCursorW(ptr::null_mut(), IDC_ARROW);
     winc.hbrBackground = GetStockObject(WHITE_BRUSH as i32) as HBRUSH;
@@ -59,9 +78,9 @@ unsafe fn create_window(class_name: &[u16]) -> HWND {
     CreateWindowExW(
         0,
         class_name.as_ptr(),
-        encode("Hello, World!").as_ptr(),
+        encode("DX12Sample").as_ptr(),
         WS_OVERLAPPEDWINDOW,
-        0, 0, 200, 200,
+        0, 0, WINDOW_WIDTH, WINDOW_HEIGHT,
         ptr::null_mut(),
         ptr::null_mut(),
         ptr::null_mut(),
@@ -69,10 +88,26 @@ unsafe fn create_window(class_name: &[u16]) -> HWND {
     )
 }
 
-unsafe extern "system" fn win_proc(hwnd: HWND, msg: UINT, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
+unsafe extern "system" fn window_procedure(hwnd: HWND, msg: UINT, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
     match msg {
         WM_DESTROY => PostQuitMessage(0),
         _ => return DefWindowProcW(hwnd, msg, w_param, l_param),
     };
     0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn some_test() {
+        let levels: [d3dcommon::D3D_FEATURE_LEVEL; 4] = [
+            d3dcommon::D3D_FEATURE_LEVEL_12_1,
+            d3dcommon::D3D_FEATURE_LEVEL_12_0,
+            d3dcommon::D3D_FEATURE_LEVEL_11_1,
+            d3dcommon::D3D_FEATURE_LEVEL_11_0
+        ];
+
+        println!("{:?}", levels);
+    }
 }
