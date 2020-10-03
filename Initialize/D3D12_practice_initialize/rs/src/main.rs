9d9d5ffd75dabe0dff7c544f931345d1b703f00e
Hajime-san <utd.c.r.d.s.a@gmail.com>
2020-10-04 01:36:43 +0900

    create IDXGIFactory4 object

--- Initialize/D3D12_practice_initialize/rs/src/main.rs
+++ Initialize/D3D12_practice_initialize/rs/src/main.rs
@@ -6,15 +6,21 @@ use winapi::{
                   GetMessageW, TranslateMessage, DispatchMessageW, MSG,
                   WM_DESTROY, PostQuitMessage, DefWindowProcW, WS_OVERLAPPEDWINDOW },
         wingdi::{ GetStockObject, WHITE_BRUSH },
-        d3d12,
-        d3d12sdklayers,
-        d3dcommon,
+        d3d12::*,
+        d3d12sdklayers::*,
+        d3dcommon::*,
     },
     shared::{
         windef::{ HWND, HBRUSH },
         minwindef::{ UINT, WPARAM, LPARAM, LRESULT },
-        winerror::{ S_OK },
+        winerror::{ S_OK, DXGI_ERROR_NOT_FOUND },
         guiddef::*,
+        dxgi::*,
+        dxgi1_2::*,
+        dxgi1_3::*,
+        dxgi1_4::*,
+        dxgiformat::*,
+        dxgitype::*,
     },
     ctypes::c_void,
 };
@@ -36,46 +42,53 @@ fn main() {
             return;
         }
 
-        let mut dev: d3d12::ID3D12Device;
+        let mut device = ptr::null_mut::<ID3D12Device>();
+        let mut dxgi_factory = ptr::null_mut::<IDXGIFactory4>();
 
 
         // initialize Direct3D device
-        let levels: [d3dcommon::D3D_FEATURE_LEVEL; 4] = [
-            d3dcommon::D3D_FEATURE_LEVEL_12_1,
-            d3dcommon::D3D_FEATURE_LEVEL_12_0,
-            d3dcommon::D3D_FEATURE_LEVEL_11_1,
-            d3dcommon::D3D_FEATURE_LEVEL_11_0
+        let levels: [D3D_FEATURE_LEVEL; 4] = [
+            D3D_FEATURE_LEVEL_12_1,
+            D3D_FEATURE_LEVEL_12_0,
+            D3D_FEATURE_LEVEL_11_1,
+            D3D_FEATURE_LEVEL_11_0
         ];
 
-        let mut feature_level : d3dcommon::D3D_FEATURE_LEVEL = 0;
-
-        const REFGUID: GUID = GUID {
-            Data1: 0x189819f1,
-            Data2: 0x1db6,
-            Data3: 0x4b57,
-            Data4: [ 0xbe, 0x54, 0x18, 0x21, 0x33, 0x9b, 0x85, 0xf7 ],
-        };
-
-        let mut pp_device = ptr::null_mut::<d3d12::ID3D12Device>();
+        let mut feature_level : D3D_FEATURE_LEVEL = 0;
 
         for lv in levels.iter() {
 
-            if d3d12::D3D12CreateDevice(
+            if D3D12CreateDevice(
                 ptr::null_mut(),
                 *lv,
-                &REFGUID,
-                &mut pp_device as *mut *mut d3d12::ID3D12Device as *mut *mut c_void
+                &IID_ID3D12Device,
+                &mut device as *mut *mut ID3D12Device as *mut *mut c_void
                 )
                  == S_OK {
-                println!("{}", "foooooo");
-
                 feature_level = *lv;
 			    break;
             }
         }
 
-        println!("{}", feature_level);
+        // return value 0 is S_OK
+        let result = CreateDXGIFactory(&IID_IDXGIFactory, &mut dxgi_factory as *mut *mut IDXGIFactory4 as *mut *mut c_void);
 
+        // iterate adapter to use
+
+        let mut adapters: Vec<IDXGIAdapter> = vec![];
+
+        let mut  tmp_adapter = ptr::null_mut::<IDXGIAdapter>();
+
+        let mut i = 0;
+
+        while IDXGIFactory::EnumAdapters(&*dxgi_factory, i, &mut tmp_adapter as *mut *mut IDXGIAdapter) != DXGI_ERROR_NOT_FOUND {
+            i += 1;
+
+            let p_desc = ptr::null_mut::<DXGI_ADAPTER_DESC>();
+
+            IDXGIAdapter::GetDesc(&*tmp_adapter, p_desc);
+
+        }
 
 
         ShowWindow(hwnd, SW_NORMAL);
@@ -95,6 +108,12 @@ fn main() {
 
 fn encode(source: &str) -> Vec<u16> {
     source.encode_utf16().chain(Some(0)).collect()
+}
+
+fn to_wide_chars(s: &str) -> Vec<u16> {
+    use std::ffi::OsStr;
+    use std::os::windows::ffi::OsStrExt;
+    OsStr::new(s).encode_wide().chain(Some(0).into_iter()).collect::<Vec<_>>()
 }
 
 unsafe fn register_wndclass(class_name: &[u16]) -> bool {
@@ -136,11 +155,11 @@ mod tests {
     use super::*;
     #[test]
     fn some_test() {
-        let levels: [d3dcommon::D3D_FEATURE_LEVEL; 4] = [
-            d3dcommon::D3D_FEATURE_LEVEL_12_1,
-            d3dcommon::D3D_FEATURE_LEVEL_12_0,
-            d3dcommon::D3D_FEATURE_LEVEL_11_1,
-            d3dcommon::D3D_FEATURE_LEVEL_11_0
+        let levels: [D3D_FEATURE_LEVEL; 4] = [
+            D3D_FEATURE_LEVEL_12_1,
+            D3D_FEATURE_LEVEL_12_0,
+            D3D_FEATURE_LEVEL_11_1,
+            D3D_FEATURE_LEVEL_11_0
         ];
 
         println!("{:?}", levels);
